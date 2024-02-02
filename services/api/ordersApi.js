0af6f214e79226f1c6388ff079a1ac0803b362c6
z9877969 <z9877969@ukr.net>
2024-02-02 17:55:45 +0200

    adds logs for test

--- services/api/ordersApi.js
+++ services/api/ordersApi.js
@@ -3,11 +3,13 @@ const { apiInstance } = require("./apiInstance");
 
 const getOrderPasswordApi = async (contractId) => {
   try {
+    console.log(`/contract/${contractId}/otp/send`, "START");
     const { data } = await apiInstance.get(`/contract/${contractId}/otp/send`, {
       params: {
         customer: true,
       },
     });
+    console.log(`/contract/${contractId}/otp/send`, data);
 
     return data;
   } catch (error) {
@@ -17,11 +19,13 @@ const getOrderPasswordApi = async (contractId) => {
 
 const checkOrderPasswordApi = async ({ contractId, customer }) => {
   try {
+    console.log(`/contract/${contractId}/otp`, "START");
     const { data } = await apiInstance.get(`/contract/${contractId}/otp`, {
       params: {
         customer,
       },
     });
+    console.log(`/contract/${contractId}/otp`, data);
 
     return data;
   } catch (error) {
@@ -31,11 +35,15 @@ const checkOrderPasswordApi = async ({ contractId, customer }) => {
 
 const updateOrderStatusApi = async ({ contractId, state }) => {
   try {
+    console.log(`/contract/${contractId}/state/${state}`, "START");
     const { data } = await apiInstance.post(
       `/contract/${contractId}/state/${state}`
     );
+    console.log(`/contract/${contractId}/state/${state}`, data);
     return data;
   } catch (error) {
+    console.log("error.request._header :>> ", error.request._header);
+    console.log("error.response :>> ", error.response);
     throw createAxiosError(error);
   }
 };
