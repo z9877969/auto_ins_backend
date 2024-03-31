e2f7b2420b8b1372a327d925e8190976dfbb31d6
z9877969 <z9877969@ukr.net>
2024-03-31 13:24:21 +0300

    adds REQUESTED and EMMITED types to vcl order request

--- controllers/ordersControllers.js
+++ controllers/ordersControllers.js
@@ -1,7 +1,7 @@
-const { ordersApi: api } = require("../services");
-const { orderCheck } = require("../constants");
-const { createError } = require("../helpers");
-const { EMMITED_ORDER_REDIRECT_URL } = require("../envConfigs");
+const { ordersApi: api } = require('../services');
+const { orderCheck } = require('../constants');
+const { createError } = require('../helpers');
+const { EMMITED_ORDER_REDIRECT_URL } = require('../envConfigs');
 
 const getOrderPassword = async (req, res, next) => {
   const { contractId } = req.params;
@@ -33,12 +33,19 @@ const checkOrderPassword = async (req, res, next) => {
 
 const updateOrderToRequestStatus = async (req, res, next) => {
   const { contractId } = req.params;
+  const { epolicy: epolicyId, vcl: vclId } = req.query;
 
   try {
     await api.updateOrderStatusApi({
-      contractId,
+      contractId: epolicyId,
       state: orderCheck.state.REQUEST,
     });
+    if (vclId) {
+      await api.updateOrderStatusApi({
+        contractId: vclId,
+        state: orderCheck.state.REQUEST,
+      });
+    }
     res.json({ contractId });
   } catch (error) {
     next(error);
@@ -47,15 +54,31 @@ const updateOrderToRequestStatus = async (req, res, next) => {
 
 const updateOrderToEmmitAndRedirect = async (req, res, next) => {
   const { contractId } = req.params;
+  const { epolice: epoliceId, vcl: vclId } = req.query;
 
   try {
     const data = await api.updateOrderStatusApi({
-      contractId,
+      contractId: contractId || epoliceId,
       state: orderCheck.state.EMITTED,
     });
 
-    if (data.id.toString() !== contractId.toString()) {
-      throw createError(400, "Поліс не укладено");
+    if (
+      data.id.toString() !== contractId.toString() &&
+      data.id.toString() !== epoliceId.toString()
+    ) {
+      throw createError(400, 'Поліс еОСЦПВ не укладено');
+    }
+
+    if (vclId) {
+      const data = await api.updateOrderStatusApi({
+        contractId: contractId || epoliceId,
+        state: orderCheck.state.EMITTED,
+      });
+      if (
+        data.id.toString() !== vclId.toString()
+      ) {
+        throw createError(400, 'Поліс ДЦВ не укладено');
+      }
     }
 
     res.redirect(EMMITED_ORDER_REDIRECT_URL);
