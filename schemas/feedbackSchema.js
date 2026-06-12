25a199a3e1a75e24d67e33d1b53bcade65f55105
z9877969 <z9877969@ukr.net>
2026-06-12 18:48:00 +0300

    increase feedback text max length from 30 to 300 characters

--- schemas/feedbackSchema.js
+++ schemas/feedbackSchema.js
@@ -2,6 +2,6 @@ const Joi = require('joi');
 
 module.exports = Joi.object({
   name: Joi.string().min(2).max(20).required(),
-  text: Joi.string().min(5).max(30).required(),
+  text: Joi.string().min(5).max(300).required(),
   rating: Joi.number().integer().min(1).max(5).required(),
 });
